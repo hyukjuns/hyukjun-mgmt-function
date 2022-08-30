5b8a9e3262448f28b60fc3494ee10c76dd9ce989
hyukjun <hyukjun1994@gmail.com>
2022-08-30 16:08:29 +0900

    add httptrigger

--- EventGridTrigger1/run.ps1
+++ EventGridTrigger1/run.ps1
@@ -2,3 +2,4 @@ param($eventGridEvent, $TriggerMetadata)
 
 # Make sure to pass hashtables to Out-String so they're logged correctly
 $eventGridEvent | Out-String | Write-Host
+$eventGridEvent | convertto-json | Write-Host
---
+++ README.md
@@ -0,0 +1,2 @@
+# Azure Function for Event Handler
+이벤트 핸들러로써 사용할 Azure Functions 예제
